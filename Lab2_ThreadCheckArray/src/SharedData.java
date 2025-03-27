23c5beac255c7ac8eb963eaa7079709f9c3c89a8
Gal mitrani <galmitrani@walla.com>
2025-03-27 10:58:49 +0200

    Added javadoc for SharedData

--- Lab2_ThreadCheckArray/src/SharedData.java
+++ Lab2_ThreadCheckArray/src/SharedData.java
@@ -1,43 +1,96 @@
+/**
+ * This class represents a shared data object that holds:
+ * - an array of integers
+ * - a boolean array (for win flags or similar purposes)
+ * - a final integer value
+ * - a flag (boolean)
+ * 
+ * Used to share data between different components or threads.
+ * 
+ * @author Noam darlinger 
+ * @author Gal mitrani
+ *  */
+ 
+
 public class SharedData 
 {
-	private int [] array;
-	private boolean [] winArray;
-	private boolean flag;
-	private final int b;
-	
-	public SharedData(int[] array, int b) {
-		
-		this.array = array;
-		this.b = b;
-	}
+    /** An array of integers */
+    private int[] array;
 
-	public boolean[] getWinArray() 
-	{
-		return winArray;
-	}
+    /** A boolean array indicating win status or similar flags */
+    private boolean[] winArray;
 
-	public void setWinArray(boolean [] winArray) 
-	{
-		this.winArray = winArray;
-	}
+    /** A flag used for signaling */
+    private boolean flag;
 
-	public int[] getArray() 
-	{
-		return array;
-	}
+    /** A constant integer value */
+    private final int b;
+    
+    
 
-	public int getB() 
-	{
-		return b;
-	}
+    /**
+     * Constructor for SharedData
+     * 
+     * @param array an array of integers
+     * @param b a constant integer value
+     */
+    public SharedData(int[] array, int b) {
+        this.array = array;
+        this.b = b;
+    }
+    
 
-	public boolean getFlag() 
-	{
-		return flag;
-	}
+    /**
+     * Gets the win array
+     * 
+     * @return a boolean array
+     */
+    public boolean[] getWinArray() {
+        return winArray;
+    }
 
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
+    /**
+     * Sets the win array
+     * 
+     * @param winArray a new boolean array
+     */
+    public void setWinArray(boolean[] winArray) {
+        this.winArray = winArray;
+    }
 
+    /**
+     * Gets the array of integers
+     * 
+     * @return an int array
+     */
+    public int[] getArray() {
+        return array;
+    }
+
+    /**
+     * Gets the constant value b
+     * 
+     * @return an integer
+     */
+    public int getB() {
+        return b;
+    }
+
+    /**
+     * Gets the flag value
+     * 
+     * @return true or false
+     */
+    public boolean getFlag() {
+        return flag;
+    }
+
+    /**
+     * Sets the flag value
+     * 
+     * @param flag the new  boolean flag value
+     */
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
